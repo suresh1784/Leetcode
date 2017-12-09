3655152a53a1d223a8e862717f364cb2b28b7cfa
Sureshbabu Rajendiran <sureshbabu.rajendiran@oracle.com>
2017-12-08 21:57:18 -0800

    DP solution with explantaion

--- src/main/java/com/fishercoder/solutions/_714.java
+++ src/main/java/com/fishercoder/solutions/_714.java
@@ -28,10 +28,11 @@ package com.fishercoder.solutions;
  */
 public class _714 {
     public static class Solution1 {
-        /**O(n) time
+        /**
+         * O(n) time
          * O(n) space
          * credit: https://discuss.leetcode.com/topic/108009/java-c-clean-code-dp-greedy
-         * */
+         */
         public int maxProfit(int[] prices, int fee) {
             int n = prices.length;
             if (n < 2) {
@@ -49,17 +50,18 @@ public class _714 {
     }
 
     public static class Solution2 {
-        /**O(n) time
+        /**
+         * O(n) time
          * O(1) space
          * credit: https://leetcode.com/articles/best-time-to-buy-and-sell-stock-with-transaction-fee/
-         *
+         * <p>
          * cash: the max profit we could have if we did not have a share of stock in hand
          * hold: the max profit we could have if we hold one share of stack in hand
-         *
+         * <p>
          * to transition from the i-th day to the i+1 th day, we have two options:
          * 1. sell our stock: cash = Math.max(cash, hold + prices[i] - fee)
          * 2. buy a stock: hold = Math.max(hold, cash - prices[i])
-         * */
+         */
         public int maxProfit(int[] prices, int fee) {
             int cash = 0;
             int hold = -prices[0];
@@ -70,4 +72,61 @@ public class _714 {
             return cash;
         }
     }
+
+
+//    Given any day I, its max profit status boils down to one of the two status below:
+//
+//            (1) buy status:
+//    buy[i] represents the max profit at day i in buy status, given that the last action you took is a buy action at day K, where K<=i. And you have the right to sell at day i+1, or do nothing.
+//            (2) sell status:
+//    sell[i] represents the max profit at day i in sell status, given that the last action you took is a sell action at day K, where K<=i. And you have the right to buy at day i+1, or do nothing.
+//
+//            Let's walk through from base case.
+//
+//    Base case:
+//    We can start from buy status, which means we buy stock at day 0.
+//    buy[0]=-prices[0];
+//    Or we can start from sell status, which means we sell stock at day 0.
+//    Given that we don't have any stock at hand in day 0, we set sell status to be 0.
+//    sell[0]=0;
+//
+//    Status transformation:
+//    At day i, we may buy stock (from previous sell status) or do nothing (from previous buy status):
+//    buy[i] = Math.max(buy[i - 1], sell[i - 1] - prices[i]);
+//    Or
+//    At day i, we may sell stock (from previous buy status) or keep holding (from previous buy status):
+//    sell[i] = Math.max(sell[i - 1], buy[i - 1] + prices[i]);
+//
+//    Finally:
+//    We will return sell[last_day] as our result, which represents the max profit at the last day, given that you took sell action at any day before the last day.
+//
+//    We can apply transaction fee at either buy status or sell status.
+//
+//    So here come our two solutions:
+//
+//    Solution I -- pay the fee when buying the stock:
+public static class Solution3 {
+    public int maxProfit1(int[] prices, int fee) {
+        if (prices.length <= 1) return 0;
+        int days = prices.length, buy[] = new int[days], sell[] = new int[days];
+        buy[0]=-prices[0]-fee;
+        for (int i = 1; i<days; i++) {
+            buy[i] = Math.max(buy[i - 1], sell[i - 1] - prices[i] - fee); // keep the same as day i-1, or buy from sell status at day i-1
+            sell[i] = Math.max(sell[i - 1], buy[i - 1] + prices[i]); // keep the same as day i-1, or sell from buy status at day i-1
+        }
+        return sell[days - 1];
+    }
+   // Solution II -- pay the fee when selling the stock:
+
+    public int maxProfit2(int[] prices, int fee) {
+        if (prices.length <= 1) return 0;
+        int days = prices.length, buy[] = new int[days], sell[] = new int[days];
+        buy[0]=-prices[0];
+        for (int i = 1; i<days; i++) {
+            buy[i] = Math.max(buy[i - 1], sell[i - 1] - prices[i]); // keep the same as day i-1, or buy from sell status at day i-1
+            sell[i] = Math.max(sell[i - 1], buy[i - 1] + prices[i] - fee); // keep the same as day i-1, or sell from buy status at day i-1
+        }
+        return sell[days - 1];
+    }
+}
 }
